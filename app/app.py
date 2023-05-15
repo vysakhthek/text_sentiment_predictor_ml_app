30e3338017468eb9384e93f06ab0c4ca92413db0
vysakhthek <vysakh.thek@gmail.com>
2023-05-16 00:00:24 +0530

    Monitor App Files updated

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import altair as alt
+from datetime import datetime
 
 
 
@@ -55,10 +56,32 @@ def main():
                 
  
     elif choice == "Monitor":
-        st.subheader("Monitor App")
+        add_page_visited_details("Monitor",datetime.now())
+		st.subheader("Monitor App")
+
+		with st.beta_expander("Page Metrics"):
+			page_visited_details = pd.DataFrame(view_all_page_visited_details(),columns=['Pagename','Time_of_Visit'])
+			st.dataframe(page_visited_details)	
+
+			pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
+			c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
+			st.altair_chart(c,use_container_width=True)	
+
+			p = px.pie(pg_count,values='Counts',names='Pagename')
+			st.plotly_chart(p,use_container_width=True)
+
+		with st.beta_expander('Emotion Classifier Metrics'):
+			df_emotions = pd.DataFrame(view_all_prediction_details(),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
+			st.dataframe(df_emotions)
+
+			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
+			pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
+			st.altair_chart(pc,use_container_width=True)	
+
     
     else:
         st.subheader("About")
+        add_page_visited_details("About",datetime.now())
 
 
 if __name__ == '__main__':
